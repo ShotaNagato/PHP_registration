9a296053c2e0ae0ea7371aef1d163c77a068f635
NagatoShota <n.shota1031@gmail.com>
2022-03-21 14:34:46 +0900

    20220321-signup_email

---
+++ mail/signup_mail.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>仮会員登録</title>
+</head>
+<body>
+    <h1>仮会員登録画面</h1>
+</body>
+</html>
